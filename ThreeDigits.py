6a724d72e862b6038677d66d0d2b3c473daa3a86
Brandon Temple <brandon.temple.97@gmail.com>
2018-04-19 20:46:29 +1000

    20180419 8:46PM

--- ThreeDigits.py
+++ ThreeDigits.py
@@ -1,6 +1,8 @@
 import sys
 
 # TODO: node object
+from typing import List
+
 
 class Node:
     def __init__(self):
@@ -9,6 +11,12 @@ class Node:
         self.children = []
         self.previous_change = None
         self.parent = None
+
+    def __str__(self):
+        return ''.join(str(e) for e in self.number)
+
+    def __repr__(self):
+        return ''.join(str(e) for e in self.number)
 
     def __eq__(self, other):
         if self.number == other.number:
@@ -21,44 +29,64 @@ class Node:
 
 # TODO
 # Add previous change to new nodes and h values
+
+def get_path(node):
+    return "Make Path"
+
 def generate_children(node):
     if node.previous_change is None:
+        new_children = []
+        print("Generating root children...")
         newChild = Node()
         newChild.number = node.number
+        print("Child 1 num before:")
+        print(newChild.number)
         newChild.number[0] -= 1
+        print("Child 1 num after:")
+        print(newChild.number)
         newChild.previous_change = 0
         newChild.parent = node
-        node.children.append(newChild)
+        new_children.append(newChild)
+        for c in new_children:
+            print(c.number)
+        node.children = new_children
         newChild = Node()
         newChild.number = node.number
         newChild.number[0] += 1
         newChild.previous_change = 0
         newChild.parent = node
-        node.children.append(newChild)
+        new_children.append(newChild)
+        for c in new_children:
+            print(c.number)
+        node.children = new_children
         newChild = Node()
         newChild.number = node.number
         newChild.number[1] -= 1
         newChild.previous_change = 1
         newChild.parent = node
-        node.children.append(newChild)
+        new_children.append(newChild)
         newChild = Node()
         newChild.number = node.number
         newChild.number[1] += 1
         newChild.previous_change = 1
         newChild.parent = node
-        node.children.append(newChild)
+        new_children.append(newChild)
         newChild = Node()
         newChild.number = node.number
         newChild.number[2] -= 1
         newChild.previous_change = 2
         newChild.parent = node
-        node.children.append(newChild)
+        new_children.append(newChild)
         newChild = Node()
         newChild.number = node.number
         newChild.number[2] += 1
         newChild.previous_change = 2
         newChild.parent = node
-        node.children.append(newChild)
+        new_children.append(newChild)
+        print("New_Children:")
+        for c in new_children:
+            print(c.number)
+        node.children = new_children
     elif node.previous_change is 0:
         newChild = Node()
         newChild.number = node.number
@@ -134,6 +162,8 @@ def generate_children(node):
         newChild.previous_change = 1
         newChild.parent = node
         node.children.append(newChild)
+    print("Returned children:")
+    print(','.join(str(e) for e in node.children))
     return node.children
 
 
@@ -143,74 +173,96 @@ def generate_children(node):
 
 #TODO
 def a_star():
+    return
 
-def bfs(root, goal):
+def bfs(root, goal, forbiddens):
     expanded = []
     fringe = []
 
     expanded.append(root)
+    print("beginning on:")
+    print(print(','.join(str(e) for e in expanded)))
+    print("Starting search...")
 
     if root.number == goal:
         path = get_path(root)
         print(path)
-        print(expanded)
+        print(','.join(str(e) for e in expanded))
         return
 
     for n in generate_children(root):
+        if (n.number in forbiddens):
+            continue
         fringe.append(n)
 
-    for n in fringe:
-        if n in expanded:
-            continue
+    for i in range(0, 50):
+        n = fringe.pop(0)
+        if n.number == goal:
+            path = get_path(n)
+            print(path)
+            print(','.join(str(e) for e in expanded))
+            return
         else:
-            expanded.append(n)
-
-            if n.number == goal:
-                path = get_path(root)
-                print(path)
-                print(expanded)
-                return
-            for m in n.children:
-                fringe.append(m)
+            for c in generate_children(n):
+                if c.number in forbiddens:
+                    continue
+                fringe.append(c)
+    print("No solution found.")
+    print("Error checking printout:")
+    print(print(','.join(str(e) for e in expanded)))
 
 
 def dfs():
+    return
 
 def ids():
+    return
 
 def greedy():
+    return
 
 def hill_climbing():
+    return
 
 #------------------------------#
 #             Main             #
 #------------------------------#
-if length(sys.argv) != 3:
+if len(sys.argv) != 3:
     print("Invalid arguments.")
     exit(0)
 search = sys.argv[1]
 file = open(sys.argv[2])
 start_state = file.readline().replace('\n', '')
 start_state = list(start_state)
-end_state = file.readline().reaplce('\n', '')
+end_state = file.readline().replace('\n', '')
 end_state = list(end_state)
 forbidden_values_readin = file.readline(3).split(',')
 forbidden_values = []
 for value in forbidden_values_readin:
     forbidden_values.append(list(value))
-decision_tree_root = build_tree(start_state, forbidden_values)
+
+for i in range(0, 3):
+    start_state[i] = int(start_state[i])
+    end_state[i] = int(end_state[i])
+for i in range(0, len(forbidden_values)):
+    for j in range(0, len(forbidden_values[i])):
+        forbidden_values[i][j] = int(forbidden_values[i][j])
+
+decision_tree_root = Node()
+decision_tree_root.number = start_state
+
 
 if search is 'B':
-    bfs(decision_tree_root, end_state)
-elif search is 'D':
-    dfs(decision_tree_root, end_state)
-elif search is 'I':
-    ids(decision_tree_root, end_state)
-elif search is 'G':
-    greedy(decision_tree_root, end_state)
-elif search is 'A':
-    a_star(decision_tree_root, end_state)
-elif search is 'H':
-    hill_climbing(decision_tree_root, end_state)
-else:
-    print("Invalid search")
+    bfs(decision_tree_root, end_state, forbidden_values)
+#elif search is 'D':
+#    dfs(decision_tree_root, end_state)
+#elif search is 'I':
+#    ids(decision_tree_root, end_state)
+#elif search is 'G':
+#    greedy(decision_tree_root, end_state)
+#elif search is 'A':
+#    a_star(decision_tree_root, end_state)
+#elif search is 'H':
+#    hill_climbing(decision_tree_root, end_state)
+#else:
+    #print("Invalid search")
